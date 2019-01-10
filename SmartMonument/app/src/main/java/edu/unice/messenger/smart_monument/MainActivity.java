975dd516756f85302504454ad8e28813515c0206
Ines NASR <ines.nasr.93@gmail.com>
2019-01-11 00:39:12 +0100

    MODIFER TAG ID

--- SmartMonument/app/src/main/java/edu/unice/messenger/smart_monument/MainActivity.java
+++ SmartMonument/app/src/main/java/edu/unice/messenger/smart_monument/MainActivity.java
@@ -1,17 +1,22 @@
 package edu.unice.messenger.smart_monument;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import edu.unice.messenger.smart_monument.NFCHelper.Utils;
 
 public class MainActivity extends Activity {
 
+    NfcAdapter nfcAdapter = null;
+    PendingIntent pendingIntent;
+    Intent myIntent;
     private String tagUID;
     private Tag tag;
 
@@ -21,6 +26,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myIntent = getIntent();
 
         btnHistoriqueVisites = (Button) findViewById(R.id.btnHistoriqueVisites);
         btnHistoriqueVisites.setOnClickListener(new View.OnClickListener() {
@@ -36,9 +42,27 @@ public class MainActivity extends Activity {
     }
 
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        nfcAdapter = NfcAdapter.getDefaultAdapter(getApplicationContext());
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).
+                addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT), 0);
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        onNewIntent(myIntent);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
     //Méthode invoquée lors de la détection d'un tag/périphérique NFC
     @Override
     public void onNewIntent(Intent intent) {
+
+
         //Détection d'un périphérique NFC (tag ou autre)
         if ((intent.getAction() != null) &&
                 ((NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction()) ||
@@ -50,6 +74,7 @@ public class MainActivity extends Activity {
             tagUID = byteArrayToHex(id).toUpperCase();
             Utils utils = new Utils();
             utils.setTagUID(tagUID);
+            Toast.makeText(getApplicationContext(), tagUID, Toast.LENGTH_LONG).show();
             // Launching the NouveauMonumentActivity
             Intent intent2 = new Intent(MainActivity.this, NouveauMonumentActivity.class);
             startActivity(intent2);
