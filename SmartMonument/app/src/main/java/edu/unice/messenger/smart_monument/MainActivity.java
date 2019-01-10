d2b3d9f48c8b8aab015b6726cda6d31c9a4270eb
Ines NASR <ines.nasr.93@gmail.com>
2019-01-10 22:44:23 +0100

    Création des layouts

--- SmartMonument/app/src/main/java/edu/unice/messenger/smart_monument/MainActivity.java
+++ SmartMonument/app/src/main/java/edu/unice/messenger/smart_monument/MainActivity.java
@@ -1,13 +1,32 @@
 package edu.unice.messenger.smart_monument;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnHistoriqueVisites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHistoriqueVisites = (Button) findViewById(R.id.btnHistoriqueVisites);
+        btnHistoriqueVisites.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Launching the HistoriqueVisitesActivity
+                Intent intent = new Intent(MainActivity.this, HistoriqueVisitesActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
